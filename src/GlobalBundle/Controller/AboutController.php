011db51af470082cf416df423f98b4608c0b0eb2
Cesar Sousa <cezzaar@gmail.com>
2016-01-23 12:02:34 -0200

    segundo commit

--- src/GlobalBundle/Controller/AboutController.php
+++ src/GlobalBundle/Controller/AboutController.php
@@ -2,14 +2,56 @@
 
 namespace GlobalBundle\Controller;
 
+use Doctrine\DBAL\Types\IntegerType;
+use GlobalBundle\Entity\About;
+use GlobalBundle\Tests\Controller\AboutControllerTest;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Tests\Extension\Core\Type\SubmitTypeTest;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AboutController extends Controller
 {
-    public function indexAction()
+    public function aboutAction()
     {
-        return $this->render('GlobalBundle:About:index.html.twig', array(
-                // ...
-            ));    }
+        $repository = $this->getDoctrine()->getEntityManager();
+        $find = $repository->getRepository('GlobalBundle:About');
 
+        return $this->render('GlobalBundle:About:index.html.twig', array('about' => $find->findAll()));
+    }
+
+    public function addAction(){
+
+        $e = new About();
+
+        $e->setName('About');
+        $e->setDescription('Well Done!, This is a tiny description.');
+        $e->setActive(1);
+
+        $repository = $this->getDoctrine()->getEntityManager();
+
+        $repository->persist($e);
+        $repository->flush();
+
+        return new Response('Now you have a new about row.');
+    }
+
+    public function newAction(Request $request){
+
+        $about = new About();
+
+        $about->setName('New About');
+        $about->setDescription('New Description');
+        $about->setActive(1);
+
+        $form = $this->createFormBuilder($about)
+            ->add('Name', TextType::class)
+            ->add('Description', TextType::class)
+            ->add('Active', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Criar Registro'))
+        ->getForm();
+
+        return $this->render(':default:index.html.twig', array('form' => $form->createView()));
+    }
 }
---
+++ src/GlobalBundle/Entity/About.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace GlobalBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * About
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="GlobalBundle\Entity\AboutRepository")
+ */
+class About
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="active", type="integer")
+     */
+    private $active;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return About
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return About
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set active
+     *
+     * @param integer $active
+     *
+     * @return About
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    /**
+     * Get active
+     *
+     * @return integer
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+}
+
